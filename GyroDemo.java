78260ca0df4e1b859c6f4ff15fe8eece0fad733d
StemRobotics <31777182+stemrobotics@users.noreply.github.com>
2017-09-08 13:05:40 -0700

    Create GyroDemo.java

---
+++ GyroDemo.java
@@ -0,0 +1,40 @@
+
+package ev3.exercises.gyroDemo;
+
+import ev3.exercises.library.*;
+import lejos.hardware.Button;
+import lejos.hardware.Sound;
+import lejos.hardware.motor.*;
+import lejos.hardware.port.*;
+import lejos.utility.Delay;
+
+public class GyroDemo
+{
+    public static void main(String[] args)
+    {
+        GyroSensor    gyro = new GyroSensor(SensorPort.S2);
+
+        System.out.println("Gyro Demo");
+        System.out.println("Press to start");
+
+        Button.LEDPattern(4);    // flash green led and
+        Sound.beepSequenceUp();    // make sound when ready.
+
+        Button.waitForAnyPress();
+
+        // run until escape button pressed.
+
+        while (Button.ESCAPE.isUp())
+        {
+            Lcd.clear(5);
+            Lcd.print(5, "angle=%d av=%.3f", gyro.getAngle(), gyro.getAngularVelocity());
+
+            Delay.msDelay(250);
+        }
+
+        // free up resources.
+        gyro.close();
+
+        Sound.beepSequence();    // we are done.
+    }
+}
