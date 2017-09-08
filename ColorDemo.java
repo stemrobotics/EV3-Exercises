4e07e9d9c360beafda7638ef1c5a343a1f3320ea
StemRobotics <31777182+stemrobotics@users.noreply.github.com>
2017-09-08 13:06:52 -0700

    Create ColorDemo.java

---
+++ ColorDemo.java
@@ -0,0 +1,85 @@
+package ev3.exercises.colorDemo;
+
+import lejos.hardware.Button;
+import lejos.hardware.Sound;
+import lejos.hardware.port.SensorPort;
+import lejos.robotics.Color;
+import lejos.utility.Delay;
+import ev3.exercises.library.ColorSensor;
+import ev3.exercises.library.Lcd;
+
+public class ColorDemo
+{
+    public static void main(String[] args)
+    {
+        ColorSensor    color = new ColorSensor(SensorPort.S3);
+
+        System.out.println("Color Demo");
+        Lcd.print(2, "Press to start");
+        
+        Button.LEDPattern(4);    // flash green led and
+        Sound.beepSequenceUp();    // make sound when ready.
+
+        Button.waitForAnyPress();
+        Button.LEDPattern(0);
+        
+        // run until escape button pressed.
+        
+        while (Button.ESCAPE.isUp())
+        {
+            Lcd.clear(4);
+            Lcd.print(4, "ambient=%.3f", color.getAmbient());
+            Delay.msDelay(250);
+        }
+
+        Delay.msDelay(1000);
+
+        color.setRedMode();
+        color.setFloodLight(Color.RED);
+        color.setFloodLight(true);
+        
+        while (Button.ESCAPE.isUp())
+        {
+            Lcd.clear(5);
+            Lcd.print(5, "red=%.3f", color.getRed());
+            Delay.msDelay(250);
+        }
+
+        Delay.msDelay(1000);
+
+        color.setRGBMode();
+        color.setFloodLight(Color.WHITE);
+        
+        Color rgb;
+        
+        while (Button.ESCAPE.isUp())
+        {
+            rgb = color.getColor();
+            
+            Lcd.clear(6);
+            Lcd.print(6, "r=%d g=%d b=%d", rgb.getRed(), rgb.getGreen(), rgb.getBlue());
+            Delay.msDelay(250);
+        }
+
+        Delay.msDelay(1000);
+
+        color.setColorIdMode();
+        color.setFloodLight(false);
+        
+        while (Button.ESCAPE.isUp())
+        {
+            Lcd.clear(7);
+            Lcd.print(7, "id=%s", ColorSensor.colorName(color.getColorID()));
+            Delay.msDelay(250);
+        }
+
+        // free up resources.
+        color.close();
+        
+        Sound.beepSequence();    // we are done.
+
+        Button.LEDPattern(4);
+        Button.waitForAnyPress();
+    }
+
+}
